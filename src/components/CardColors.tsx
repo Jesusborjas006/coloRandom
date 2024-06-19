f877b974fc510c56461be25c93c18402ee071e34
Jesusborjas006 <jesusbor006@gmail.com>
2024-06-19 17:09:02 -0500

    Adjust initial state data by switching to an array of objects

--- src/components/CardColors.tsx
+++ src/components/CardColors.tsx
@@ -1,43 +1,65 @@
 import { useState } from "react";
 import { generateHexCode } from "../utils";
 import lockIcon from "../assets/locked.png";
+import unlockIcon from "../assets/unlocked.png";
 
 export const CardColors = () => {
-  const [hexCodes, setHexCodes] = useState({
-    card1: generateHexCode(),
-    card2: generateHexCode(),
-    card3: generateHexCode(),
-    card4: generateHexCode(),
-    card5: generateHexCode(),
-  });
+  const [hexCodes, setHexCodes] = useState([
+    {
+      id: 1,
+      color: generateHexCode(),
+      locked: false,
+    },
+    {
+      id: 2,
+      color: generateHexCode(),
+      locked: false,
+    },
+    {
+      id: 3,
+      color: generateHexCode(),
+      locked: false,
+    },
+    {
+      id: 4,
+      color: generateHexCode(),
+      locked: false,
+    },
+    {
+      id: 5,
+      color: generateHexCode(),
+      locked: false,
+    },
+  ]);
 
-  const handleOnClick = () => {
-    setHexCodes({
-      card1: generateHexCode(),
-      card2: generateHexCode(),
-      card3: generateHexCode(),
-      card4: generateHexCode(),
-      card5: generateHexCode(),
-    });
-  };
+  // const handleOnClick = () => {
+  //   setHexCodes({
+  //     card1: generateHexCode(),
+  //     card2: generateHexCode(),
+  //     card3: generateHexCode(),
+  //     card4: generateHexCode(),
+  //     card5: generateHexCode(),
+  //   });
+  // };
 
-  const cardElements = Object.entries(hexCodes).map(([key, value]) => (
-    <div key={key}>
+  const cardElements = hexCodes.map((card) => (
+    <div key={card.id}>
       <div
         className="h-[150px] w-[150px] border-4 border-black relative"
-        style={{ backgroundColor: `#${value}` }}
+        style={{ backgroundColor: `#${card.color}` }}
       >
         <img
           className=" absolute bottom-2 right-2"
-          src={lockIcon}
+          src={unlockIcon}
           alt="lock icon"
           width={30}
         />
       </div>
 
-      <h3 className="mt-2 font-semibold text-lg">{"#" + value}</h3>
+      <h3 className="mt-2 font-semibold text-lg">{"#" + card.color}</h3>
     </div>
   ));
+  console.log(cardElements);
 
   return (
     <>
@@ -46,7 +68,7 @@ export const CardColors = () => {
       </section>
       <button
         className="bg-black text-white px-6 py-2 rounded-md text-xl mt-10"
-        onClick={handleOnClick}
+        // onClick={handleOnClick}
       >
         New Palette
       </button>
