67a96be364f416095d8162cd4c17af0da1f7a8e7
Jesusborjas006 <jesusbor006@gmail.com>
2024-06-19 17:23:35 -0500

    Function is fixed to display new color palette

--- src/components/CardColors.tsx
+++ src/components/CardColors.tsx
@@ -32,15 +32,13 @@ export const CardColors = () => {
     },
   ]);
 
-  // const handleOnClick = () => {
-  //   setHexCodes({
-  //     card1: generateHexCode(),
-  //     card2: generateHexCode(),
-  //     card3: generateHexCode(),
-  //     card4: generateHexCode(),
-  //     card5: generateHexCode(),
-  //   });
-  // };
+  const handleOnClick = () => {
+    const newHexCode = hexCodes.map((card) => {
+      return { ...card, color: generateHexCode() };
+    });
+
+    setHexCodes(newHexCode);
+  };
 
   const cardElements = hexCodes.map((card) => (
     <div key={card.id}>
@@ -59,7 +57,6 @@ export const CardColors = () => {
       <h3 className="mt-2 font-semibold text-lg">{"#" + card.color}</h3>
     </div>
   ));
-  console.log(cardElements);
 
   return (
     <>
@@ -68,7 +65,7 @@ export const CardColors = () => {
       </section>
       <button
         className="bg-black text-white px-6 py-2 rounded-md text-xl mt-10"
-        // onClick={handleOnClick}
+        onClick={handleOnClick}
       >
         New Palette
       </button>
